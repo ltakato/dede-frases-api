bbcb0d0bda5f09efe1971c5cbc3273b9ff954a54
katako <ltakato@hotmail.com>
2019-07-26 11:49:54 -0300

    feat: add cors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(cors());
 
 app.use(bodyParser.json());
 
