fe8ad0ed7f1032b99694074a4074ccad8743cdfd
katako <ltakato@hotmail.com>
2019-10-25 11:58:40 -0300

    feat: add frases update

--- src/controllers/Frases.js
+++ src/controllers/Frases.js
@@ -14,6 +14,22 @@ module.exports = {
     return res.json(frase);
   },
 
+  async update(req, res) {
+    const { body, params } = req;
+
+    try {
+      const product = await Frase.findByIdAndUpdate(params.id, body, { new: true });
+
+      if(product === null) {
+        res.status(404).send({ message: 'Frase não encontrada para o id informado'});
+      } else {
+        res.json(product);
+      }
+    } catch(e) {
+      res.status(400).send({ message: e });
+    }
+  },
+
   async delete(req, res) {
     await Frase.remove({ _id: req.params.id });
 
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ const FrasesController = require('./controllers/Frases');
 const routes = new express.Router();
 
 routes.get('/frases', FrasesController.index);
+routes.put('/frases/:id', FrasesController.update);
 routes.post('/frases', FrasesController.store);
 routes.delete('/frases/:id', FrasesController.delete);
 
